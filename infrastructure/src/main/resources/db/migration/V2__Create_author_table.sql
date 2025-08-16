acdf00cb4900a1be1e8d34cb8fc2f134bbf77f13
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-16 10:45:01 +0900

    feat: 著者テーブルを作成
        - 著者情報を管理するためのテーブルを作成し、カラムにコメントを追加

---
+++ infrastructure/src/main/resources/db/migration/V2__Create_author_table.sql
@@ -0,0 +1,13 @@
+-- 著者テーブル
+CREATE TABLE IF NOT EXISTS author (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL,
+    birth_date  DATE NOT NULL
+);
+
+-- テーブルとカラムにコメントを追加
+COMMENT ON TABLE author IS '著者情報を管理するテーブル';
+COMMENT ON COLUMN author.id IS '著者ID（主キー）';
+COMMENT ON COLUMN author.name IS '著者名';
+COMMENT ON COLUMN author.birth_date IS '生年月日';
+
