2388e9ca97543a142edd8fded286552e88d50885
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-17 00:45:28 +0900

    feat: 著者登録APIのUsecaseを実装

---
+++ usecase/src/main/kotlin/com/bookmanagementsystem/usecase/author/register/CreateAuthorUsecase.kt
@@ -0,0 +1,28 @@
+package com.bookmanagementsystem.usecase.author.register
+
+import com.bookmanagementsystem.domain.author.Author
+import com.bookmanagementsystem.domain.author.AuthorRepository
+import com.bookmanagementsystem.usecase.author.AuthorDto
+import org.springframework.stereotype.Service
+
+@Service
+class CreateAuthorUsecase(private val repository: AuthorRepository) {
+    /**
+     * 著者を登録する
+     */
+    fun execute(command: CreateAuthorCommand): AuthorDto {
+        val author = repository.insert(toEntity(command))
+        return toDto(author)
+    }
+
+    private fun toEntity(command: CreateAuthorCommand) = Author(
+        name = command.name,
+        birthDate = command.birthDate
+    )
+
+    private fun toDto(author: Author) = AuthorDto(
+        id = author.id!!,
+        name = author.name,
+        birthDate = author.birthDate
+    )
+}
