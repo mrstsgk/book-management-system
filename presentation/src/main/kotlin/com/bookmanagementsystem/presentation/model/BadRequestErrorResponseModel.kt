d63856148ea8082afbe9f3d162e3f7e87be1d9ba
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-17 17:47:54 +0900

    feat: リクエストのバリデーションエラー用のレスポンスモデルを追加     - BadRequestErrorResponseModelを作成し、OpenAPI仕様にバリデーションエラーのレスポンスを追加

---
+++ presentation/src/main/kotlin/com/bookmanagementsystem/presentation/model/BadRequestErrorResponseModel.kt
@@ -0,0 +1,19 @@
+package com.bookmanagementsystem.presentation.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Past
+import jakarta.validation.Valid
+import com.bookmanagementsystem.usecase.validation.PastOnly
+
+/**
+ * リクエストのバリデーションエラー
+ */
+data class BadRequestErrorResponseModel(
+    @field:Valid
+    @get:JsonProperty("errors") val errors: kotlin.collections.List<ErrorModel>?
+) {
+
+}
