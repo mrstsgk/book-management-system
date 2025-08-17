7d31a939cc7d6212eca0cb32d56c4b14036e43ee
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-17 18:13:48 +0900

    feat: リソースが見つからない場合の例外処理を追加     - NoSuchElementExceptionを処理するためのhandleNotFoundメソッドを実装し、NotFoundErrorResponseModelを返すようにした

--- presentation/src/main/kotlin/com/bookmanagementsystem/presentation/handler/GlobalExceptionHandler.kt
+++ presentation/src/main/kotlin/com/bookmanagementsystem/presentation/handler/GlobalExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.bookmanagementsystem.presentation.handler
 
 import com.bookmanagementsystem.presentation.model.BadRequestErrorResponseModel
 import com.bookmanagementsystem.presentation.model.ErrorModel
+import com.bookmanagementsystem.presentation.model.NotFoundErrorResponseModel
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
-
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleValidationException(ex: MethodArgumentNotValidException): ResponseEntity<BadRequestErrorResponseModel> {
         val errors = ex.bindingResult.fieldErrors.map { fieldError ->
@@ -22,6 +22,18 @@ class GlobalExceptionHandler {
 
         val errorResponse = BadRequestErrorResponseModel(errors = errors)
         return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<NotFoundErrorResponseModel> {
+        val errorModel = ErrorModel(
+            code = "NOT_FOUND",
+            message = e.message ?: "リソースが見つかりません"
+        )
+        val error = NotFoundErrorResponseModel(
+            errors = listOf(errorModel)
+        )
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error)
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
