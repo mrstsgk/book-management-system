201826d5cefa66689c853f9b25138b12093696e1
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-17 17:34:17 +0900

    feat: 生年月日用のカスタムバリデーターを追加     - 生年月日が現在の日付よりも過去であることを検証するアノテーションとバリデーターを実装

---
+++ usecase/src/main/kotlin/com/bookmanagementsystem/usecase/validation/PastOnly.kt
@@ -0,0 +1,35 @@
+package com.bookmanagementsystem.usecase.validation
+
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import java.time.LocalDate
+import kotlin.reflect.KClass
+
+/**
+ * 現在の日付よりも過去であることを検証するアノテーション
+ * 当日は含まない
+ */
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY_GETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [PastOnlyValidator::class])
+@MustBeDocumented
+annotation class PastOnly(
+    val message: String = "生年月日は現在の日付よりも過去である必要があります",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
+
+/**
+ * PastOnlyアノテーションのバリデーター
+ */
+class PastOnlyValidator : ConstraintValidator<PastOnly, LocalDate?> {
+
+    override fun isValid(value: LocalDate?, context: ConstraintValidatorContext?): Boolean {
+        if (value == null) {
+            return true // nullは他のバリデーションで処理
+        }
+        return value.isBefore(LocalDate.now())
+    }
+}
