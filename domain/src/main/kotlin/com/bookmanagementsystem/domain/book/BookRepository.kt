42a1e0beef8df7b0a5ff7615d602a05e94f9f7bc
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-18 13:45:54 +0900

    docs: getBookPublishStatusByIdメソッドに例外の説明を追加
        - 指定されたIDの書籍が存在しない場合にNoSuchElementExceptionをスローすることを明記

--- domain/src/main/kotlin/com/bookmanagementsystem/domain/book/BookRepository.kt
+++ domain/src/main/kotlin/com/bookmanagementsystem/domain/book/BookRepository.kt
@@ -23,6 +23,8 @@ interface BookRepository {
      * 書籍の存在はこのユースケース内で保証されており、存在しないことはドメイン上ありえない前提であるため、
      * Optional や null を返す `find` ではなく、存在が確定している `get` を用いている。
      * 万が一存在しなければシステムの不整合として例外を投げることで早期に検知する意図がある。
+     *
+     * @throws NoSuchElementException 指定されたIDの書籍が存在しない場合
      */
     fun getBookPublishStatusById(id: ID<Book>): BookPublishStatus
 }
