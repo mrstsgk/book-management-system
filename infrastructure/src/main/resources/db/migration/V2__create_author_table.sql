575a4696483795a198231b6bcda434ab22d014b3
mrstsgk <s.m.0819aaa@gmail.com>
2025-08-18 20:20:26 +0900

    refactor: 書籍、著者、書籍著者テーブルにバージョンカラムを追加
        - 楽観的ロック用のバージョン管理を実現するため、各テーブルにversionカラムを追加

--- infrastructure/src/main/resources/db/migration/V2__create_author_table.sql
+++ infrastructure/src/main/resources/db/migration/V2__create_author_table.sql
@@ -2,7 +2,8 @@
 CREATE TABLE IF NOT EXISTS author (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(100) NOT NULL,
-    birth_date  DATE
+    birth_date  DATE,
+    version     INTEGER
 );
 
 -- テーブルとカラムにコメントを追加
@@ -10,4 +11,5 @@ COMMENT ON TABLE author IS '著者情報を管理するテーブル';
 COMMENT ON COLUMN author.id IS '著者ID（主キー）';
 COMMENT ON COLUMN author.name IS '著者名';
 COMMENT ON COLUMN author.birth_date IS '生年月日';
+COMMENT ON COLUMN author.version IS 'バージョン（楽観的ロック用）';
 
--- infrastructure/src/main/resources/db/migration/V3__create_book_author_table.sql
+++ infrastructure/src/main/resources/db/migration/V3__create_book_author_table.sql
@@ -2,6 +2,7 @@
 CREATE TABLE book_author (
     book_id   INTEGER NOT NULL REFERENCES book(id),
     author_id INTEGER NOT NULL REFERENCES author (id),
+    version   INTEGER,
     PRIMARY KEY (book_id, author_id)
 );
 
@@ -9,3 +10,4 @@ CREATE TABLE book_author (
 COMMENT ON TABLE book_author IS '書籍と著者の中間テーブル（多対多対応）';
 COMMENT ON COLUMN book_author.book_id IS '書籍ID（外部キー）';
 COMMENT ON COLUMN book_author.author_id IS '著者ID（外部キー）';
+COMMENT ON COLUMN book_author.version IS 'バージョン（楽観的ロック用）';
